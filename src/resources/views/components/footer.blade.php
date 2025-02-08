3755e24f76e3d751e69ccaa67fdb3b2d9a42ad06
rei <idareiya123@gmail.com>
2025-02-08 21:42:00 +0900

    [feat]フッター

---
+++ src/resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-[#0078D7] p-4 md:p-6 text-white">
+    <div class="text-center">
+        <div><a class="text-3xl font-bold text-white hover:text-[#00BFFF]" href="{{ route('top') }}">Tech Draft AI</a></div>
+        <nav class="font-bold mt-4">
+            <ul class="flex gap-6 justify-center">
+                <li><a href="" class="hover:text-[#00BFFF] transition duration-200">トップ</a></li>
+                <li><a href="" class="hover:text-[#00BFFF] transition duration-200">プライバシーポリシー</a></li>
+                <li><a href="" class="hover:text-[#00BFFF] transition duration-200">利用規約</a></li>
+            </ul>
+        </nav>
+        <p class="text-sm mt-4 md:mt-6">Copyright © Tech Draft AI All Rights Reserved.</p>
+    </div>
+</footer>
