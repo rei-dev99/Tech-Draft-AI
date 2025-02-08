74a2375df869e13620196fcf235679c6dcaa47b1
rei <idareiya123@gmail.com>
2025-02-08 21:42:26 +0900

    [feat]トップページ作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('test', function () {
-    return view('test');
-});
+    return view('top');
+})->name('top');
