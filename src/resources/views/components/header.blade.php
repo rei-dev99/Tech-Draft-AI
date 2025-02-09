498ddafba45b58c90899057c53986eb1223fcb55
rei <idareiya123@gmail.com>
2025-02-09 10:04:04 +0900

    [fix] 新規登録/ログインのリンク先設定

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -4,8 +4,8 @@
         <nav>
             <ul class="flex items-center gap-6">
                 <li><a href="{{ route('top') }}" class="text-white hover:text-[#00BFFF] transition duration-200">トップ</a></li>
-                <li><a href="" class="text-white hover:text-[#00BFFF] transition duration-200">新規登録</a></li>
-                <li><a href="" class="text-white hover:text-[#00BFFF] transition duration-200">ログイン</a></li>
+                <li><a href="{{ route('register') }}" class="text-white hover:text-[#00BFFF] transition duration-200">新規登録</a></li>
+                <li><a href="{{ route('login') }}" class="text-white hover:text-[#00BFFF] transition duration-200">ログイン</a></li>
             </ul>
         </nav>
         <button class="bg-[#00BFFF] text-white hover:bg-[#fff] p-2 rounded-md transition duration-200">記事を書く</button>
