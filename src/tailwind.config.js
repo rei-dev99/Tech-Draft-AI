f104c476549f6673290ee4231aa39e9f24a25bd4
rei <idareiya123@gmail.com>
2025-02-16 06:23:39 +0900

    [add] colors追加

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -15,6 +15,13 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            'main': '#333333', // 文章 黒
+            'white': '#FFFFFF', // 文章 白
+            'primary': '#0078D7', // メイン背景 青
+            'sub': '#E6E6E6', // サブ背景 シルバー
+            'accent': '#00BFFF', // アクセント 水色
+        },
     },
 
     plugins: [forms],
